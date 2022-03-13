520a81526e440c8f9d561e96852ecefbba480e59
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-03-13 23:19:12 +0530

    Update board.py

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 def dropCoin(board, column, player):
     if column < 1 or column > 7:
         return None
-    else:   
+    else:
         for row in range(5, -1, -1):
             if board[row][column - 1] == 0:
                 board[row][column - 1] = player
